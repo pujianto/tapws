786fd8e9d0ef186d54fd9089f961a4225356371d
Pujianto <hello@pujianto.net>
2022-05-14 09:24:16 +0700

    simplify

--- tapws/services/dhcp.py
+++ tapws/services/dhcp.py
@@ -19,55 +19,22 @@ class NotImplementedError(Exception):
 
 class DHCPServerProtocol(asyncio.DatagramProtocol):
 
-    def __init__(self):
+    def __init__(self, server):
         self._handlers = {
             'DHCPDISCOVER': self.sendOffer,
             'DHCPREQUEST': self.sendAck,
         }
-
-    packets = []
+        self.server = server
 
     def sendOffer(self, packet):
-        option_list = dhcppython.options.OptionList()
-        option_list.insert(
-            0, dhcppython.options.MessageType(code=53, length=1, data=b'\x02'))
 
-        option_list.insert(
-            1,
-            dhcppython.options.SubnetMask(
-                code=1,
-                length=4,
-                data=ipaddress.IPv4Address('255.255.255.0').packed))
-        option_list.insert(
-            2,
-            dhcppython.options.RenewalTime(code=58,
-                                           length=4,
-                                           data=b'\x00\x01Q\x80'))
-        option_list.insert(
-            3,
-            dhcppython.options.RebindingTime(code=59,
-                                             length=4,
-                                             data=b'\x00\x01Q\x80'))
-        option_list.insert(
-            4,
-            dhcppython.options.IPAddressLeaseTime(code=51,
-                                                  length=4,
-                                                  data=b'\x00\x01Q\x80'))
-        option_list.insert(
-            5,
-            dhcppython.options.ServerIdentifier(
-                code=54,
-                length=4,
-                data=ipaddress.IPv4Address('10.11.12.1').packed))
-        option_list.insert(
-            6, dhcppython.options.End(code=255, length=0, data=b''))
+        resp = DHCPPacket.Offer(
+            'ff:ff:ff:ff:ff:ff',
+            tx_id=packet.xid,
+            seconds=0,
+            yiaddr=ipaddress.IPv4Address('10.11.12.3').packed,
+        )
 
-        resp = DHCPPacket.Offer(packet.chaddr,
-                                seconds=0,
-                                tx_id=packet.xid,
-                                use_broadcast=True,
-                                yiaddr=ipaddress.IPv4Address('10.11.12.3'),
-                                option_list=option_list)
         return resp
 
     def sendAck(self, packet):
@@ -91,44 +58,47 @@ class DHCPServerProtocol(asyncio.DatagramProtocol):
 
     def connection_made(self, transport):
         self.transport = transport
+        logging.info('Connection made')
 
     def datagram_received(self, data, addr):
         h, p = addr
         logging.debug(f'HOST {h} PORT {p}')
 
         try:
-            packet = DHCPPacket.from_bytes(data)
-            logging.debug(f'received dhcp packet: {packet}')
+            packet = DHCPPacket.from_bytes(bytes(data))
+            logging.debug(f'received dhcp packet:')
+            logging.debug(packet.view_packet())
             response = self._build_response(packet)
             if response:
-                logging.debug(f'our resp: {response}')
+                logging.debug(f'our resp:')
+                logging.debug(f'{response.view_packet()}')
                 self.transport.sendto(packet.asbytes, ('255.255.255.255', p))
 
         except Exception as e:
             logging.warning(f'Error parsing DHCP packet: {e}')
+            raise e
 
 
 class DhcpServer(BaseService):
 
-    def __init__(self, ip, port):
+    def __init__(self, ip, port, tap_name=b'tap0'):
         self.ip = ip
         self.port = port
         self.loop = asyncio.get_running_loop()
+        self.tap_name = tap_name
         asyncio.create_task(self._ainit())
 
     async def _ainit(self):
 
         logging.info('Starting DHCP service')
-        factory = partial(DHCPServerProtocol)
+        factory = partial(DHCPServerProtocol, self)
         self.transport, self.protocol = await self.loop.create_datagram_endpoint(
             lambda: factory(),
-            local_addr=(self.ip, self.port),
-            family=socket.AF_INET,
+            local_addr=('0.0.0.0', self.port),
             allow_broadcast=True)
 
-        sock = self.transport.get_extra_info("socket")
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        self.transport.get_extra_info('socket').setsockopt(
+            socket.SOL_SOCKET, 25, self.tap_name)
 
         name = '%s:%d' % self.transport.get_extra_info('socket').getsockname()
         logging.info(f'DHCP listening on {name}')

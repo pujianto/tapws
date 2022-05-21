30bca8da226719c4e3d7896098b115104793da3c
Pujianto <hello@pujianto.net>
2022-05-22 00:02:39 +0700

    update utils

--- tapws/utils.py
+++ tapws/utils.py
@@ -4,5 +4,11 @@
 import macaddress
 
 
-def format_mac(data: bytes) -> str | ValueError:
+def format_mac(data: bytes) -> str:
+    """
+    Format MAC address from bytes to string.
+    :return: MAC address in format xx:xx:xx:xx:xx:xx
+    :rtype: str
+    :exception: ValueError
+    """
     return str(macaddress.MAC(data)).lower().replace('-', ':')

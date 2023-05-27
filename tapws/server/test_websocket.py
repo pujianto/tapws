5689f4dee06bc2c16970336b9478ce43cc1eeb3b
Pujianto <hello@pujianto.net>
2023-05-28 01:59:59 +0700

    feat: add more tests

--- tapws/server/test_websocket.py
+++ tapws/server/test_websocket.py
@@ -1,34 +1,79 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import asyncio
 import unittest
 import unittest.mock
+from websockets import exceptions as websockets_exceptions
+from websockets import frames
 from .websocket import WebSocket
 
 
-class TestWebSocket(unittest.IsolatedAsyncioTestCase):
-    def setUp(self) -> None:
-        super().setUp()
-        ws_server = unittest.mock.AsyncMock()
-        ws_server.close = self.sync_helper
-        self.ws_factory = unittest.mock.AsyncMock(side_effect=[ws_server])
+class MockWsFactory(object):
+    msg = b"\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xffzzzzz"
 
-        self.ws = WebSocket(
+    def __init__(self, handler, *args, **kwargs):
+        self.handler = handler
+
+        self.m = MockWs(self.handler, self.msg)
+
+    def __await__(self):
+        return self.wait().__await__()
+
+    async def wait(self):
+        await self.m
+        return self.m
+
+
+class MockWs(object):
+    def __init__(self, handler, msg, *args, **kwargs):
+        self.msg = msg
+        self.handler = handler
+
+    async def mock_messages(self):
+        yield self.msg
+
+    async def mock_connection_closed(self):
+        raise websockets_exceptions.ConnectionClosed(None, None)
+        yield
+
+    async def mock_unknown_exception(self):
+        raise Exception("unknown")
+        yield
+
+    def __await__(self):
+        tasks = []
+        tasks.append(asyncio.create_task(self.handler(self.mock_connection_closed())))
+        tasks.append(asyncio.create_task(self.handler(self.mock_unknown_exception())))
+        tasks.append(asyncio.create_task(self.handler(self.mock_messages())))
+        result = asyncio.gather(*tasks)
+        yield from result
+
+    async def wait_closed(self):
+        ...
+
+    def close(self):
+        ...
+
+
+class TestWebSocket(unittest.IsolatedAsyncioTestCase):
+    async def callback_helper(self, message: bytes):
+        self.assertEqual(MockWsFactory.msg, message)
+
+    async def testStartStop(self):
+        ws = WebSocket(
             self.callback_helper,
             "0.0.0.0",
             123,
-            ws_factory_cls=self.ws_factory,  # type: ignore
+            ws_factory_cls=MockWsFactory,
         )
 
-    def sync_helper(self):
-        return None
+        await ws.start()
+        self.assertIsNotNone(ws.ws_server)
+        conn = unittest.mock.AsyncMock()
+        conn.mac = "ff:ff:ff:ff:ff:ff"
+        with unittest.mock.patch.object(ws, "connections", [conn]):
+            ws.broadcast(MockWsFactory.msg)
+            conn.websocket.send.assert_called_once_with(message=MockWsFactory.msg)
 
-    def callback_helper(self, message: bytes):
-        pass
-
-    async def testStartStop(self):
-        self.assertIsNone(self.ws.ws_server)
-        await self.ws.start()
-        self.assertIsNotNone(self.ws.ws_server)
-        await self.ws.stop()
-        self.assertIsNone(self.ws.ws_server)
+        await ws.stop()
